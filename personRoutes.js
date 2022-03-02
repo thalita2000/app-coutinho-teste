b0da9ca26b957d73fa61ecfeab07c44482ba5ca7
Thalita Coutinho <94943936+thalita2000@users.noreply.github.com>
2022-03-02 11:33:34 -0300

    Add files via upload

---
+++ personRoutes.js
@@ -0,0 +1,111 @@
+const router = require('express').Router();
+const Person = require('../models/Person');
+const Utils = require('../utils');
+
+router.post('/', Utils.checkToken, async (req, res) => {
+    const { name, avatar, job, github, linkedin, description } = req.body;
+    if (!name) {
+        res.status(422).json({ error: 'O nome é obrigatório' });
+        return;
+    } else if (!job) {
+        res.status(422).json({ error: 'O job é obrigatório' });
+        return;
+    } else if (!github) {
+        res.status(422).json({ error: 'O github é obrigatório' });
+        return;
+    } else if (!description) {
+        res.status(422).json({ error: 'A descrição é obrigatório' });
+        return;
+    } else if (!avatar) {
+        res.status(422).json({ error: 'O avatar é obrigatório' });
+        return;
+    } else if (!avatar.includes('http')) {
+        res.status(422).json({ error: 'url do avatar é inválida' });
+        return;
+    } else if (!linkedin) {
+        res.status(422).json({ error: 'O linkedin é obrigatório' });
+        return;
+    } else if (!github.includes('https://github.com/')) {
+        res.status(422).json({ error: 'url do github é inválida' });
+        return;
+    } else if (!linkedin.includes('https://www.linkedin.com/')) {
+        res.status(422).json({ error: 'url do linkedin é inválida' });
+        return;
+    }
+    const person = {
+        name,
+        job,
+        github,
+        linkedin,
+        description,
+        avatar,
+    };
+    try {
+        await Person.create(person);
+        res.status(201).json({ message: 'Registro inserido com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+router.get('/', Utils.checkToken, async (req, res) => {
+    try {
+        const persons = await Person.find();
+        res.status(200).json(persons);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+router.get('/:id', Utils.checkToken, async (req, res) => {
+    const id = req.params.id;
+    try {
+        const person = await Person.findOne({ _id: id });
+        if (!person) {
+            res.status(422).json({ message: 'Registro não encontrado' });
+            return;
+        }
+        res.status(200).json(person);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+router.patch('/:id', Utils.checkToken, async (req, res) => {
+    const id = req.params.id;
+    const { name, avatar, job, github, linkedin, description } = req.body;
+    const person = {
+        name,
+        job,
+        github,
+        linkedin,
+        description,
+        avatar,
+    };
+    try {
+        const updatePerson = await Person.updateOne({ _id: id }, person);
+        if (updatePerson.matchedCount === 0) {
+            res.status(422).json({ error: 'Registro não encontrado' });
+            return;
+        }
+        res.status(200).json({ message: 'Registro editado com sucesso', person });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+router.delete('/:id', Utils.checkToken, async (req, res) => {
+    const id = req.params.id;
+    try {
+        const person = await Person.findOne({ _id: id });
+        if (!person) {
+            res.status(422).json({ error: 'Registro não encontrado' });
+            return;
+        }
+        await Person.deleteOne({ _id: id });
+        res.status(200).json({ message: 'Registro deletado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+module.exports = router;
